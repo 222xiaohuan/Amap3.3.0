80eba8de38ab3c520a33524179a39806393311a8
nick.gao <nick.gao@ringcentral.com>
2016-04-08 17:27:09 +0800

    delete comment

--- AMap3DDemo/app/src/main/java/com/amap/map3d/demo/basic/BasicMapActivity.java
+++ AMap3DDemo/app/src/main/java/com/amap/map3d/demo/basic/BasicMapActivity.java
@@ -25,13 +25,7 @@ public class BasicMapActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.basicmap_activity);
-	    /*
-         * 设置离线地图存储目录，在下载离线地图或初始化地图设置;
-         * 使用过程中可自行设置, 若自行设置了离线地图存储的路径，
-         * 则需要在离线地图下载和使用地图页面都进行路径设置
-         * */
-	    //Demo中为了其他界面可以使用下载的离线地图，使用默认位置存储，屏蔽了自定义设置
-      //  MapsInitializer.sdcardDir =OffLineMapUtils.getSdCacheDir(this);
+
 		mapView = (MapView) findViewById(R.id.map);
 		mapView.onCreate(savedInstanceState);// 此方法必须重写
 	 
